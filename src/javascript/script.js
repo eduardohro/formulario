07dbf7978c9f9d7cfdf1339b01f088d50904207c
Eduardo Rodrigues Oliveira <159492869+eduardohro@users.noreply.github.com>
2025-03-04 20:16:21 -0300

    Update script

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,3 +1,25 @@
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const name = document.querySelector('#name');
+    const inputBox = name.closest('.input-box');
+    const nameValue = name.value;
+
+    const errorIcon = '<i class='
+
+    const errorSpan = inputBox.querySelector('.error');
+
+    if (isEmpty(name)) {
+
+    }
+})
+
+function isEmpty(value) {
+    return value === '';
+}
+
 const passwordIcons = document.querySelectorAll('.password-icon');
 
 passwordIcons.forEach(icon => {
